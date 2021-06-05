bf165e872524e30dde6dce2365d5e93b38204bf9
MarkInada <makotoinada132@gmail.com>
2021-06-05 19:58:04 +0900

    update install_requires

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/MarkInada/DHT11_Python_Jetson",
     packages=find_packages(),
-    install_requires=["RPi.GPIO"],
+    install_requires=["Jetson.GPIO"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
